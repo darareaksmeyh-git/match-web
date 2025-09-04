00890d0f33d99f7417b5ff26f0b012c7f511787b
darareaksmey-heng <darareaksmey.heng@mjqeducation.edu.kh>
2025-09-04 11:59:37 +0700

    set up match section

---
+++ server/api/routes.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(() => {
+  return [
+    { id: '1', teamA: 'Flash Kh', teamB: 'See You Soon', img: '...', title: 'Match 1', live: true },
+    { id: '2', teamA: 'CFU Gaming', teamB: 'Galaxy Legends', img: '...', title: 'Match 2', button: 'noti' },
+    { id: '3', teamA: 'Team Max', teamB: 'Pro Esports', img: '...', title: 'Match 3', button: 'mute' },
+    { id: '4', teamA: 'Team TDN', teamB: 'Logic Esports', img: '...', title: 'Match 4', live: true },
+  ]
+})
